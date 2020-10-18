ba6eb0f4c05f928bc7fdd35ee8b9277a2f923b45
Hayder Jebur <hayderjabur1983@gmail.com>
2020-10-18 12:20:12 -0700

    deploy to heroku

---
+++ public/build/precache-manifest.4fd0035eec4e2e0e209cb24e739b2d7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e43ecc156d999c5378c1a5596c3669",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa296a87ea0fd02defe9",
+    "url": "/static/css/main.07d50d6b.chunk.css"
+  },
+  {
+    "revision": "2540beffe0ad0a79a507",
+    "url": "/static/js/2.f35ba3a8.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.f35ba3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa296a87ea0fd02defe9",
+    "url": "/static/js/main.abf74741.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);

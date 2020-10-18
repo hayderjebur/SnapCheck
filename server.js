8f6e56a938b30280778e9102444b75e6a5f06441
Hayder Jebur <hayderjabur1983@gmail.com>
2020-10-18 08:49:28 -0700

    add start server

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mood on port ${PORT}`.yellow.bold
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mood on port ${PORT}`)
 );
